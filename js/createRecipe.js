45a148f11677860f4b8decfcce404b2363c31344
michael <Michael.Schlack@gmail.com>
2023-10-09 20:56:30 +0200

    added localstorageService and basic creating Form without errors

--- js/createRecipe.js
+++ js/createRecipe.js
@@ -1,6 +1,44 @@
-import {getRecipeListFromLocalStorage,storeRecipeListInLocalStorage} from "./localStorageService.js";
-const form = document.getElementById("createRecipe");
+import {getRecipeListFromLocalStorage, storeRecipeListInLocalStorage} from "./localStorageService.js";
 
+const form = document.getElementById("createRecipe");
+const addIngredientButton = document.getElementById("add-ingredient");
+
+
+const createIngredientInput = () => {
+    const units = ["g", "kg", "ml", "l", "Stk.", "TL", "EL", "Prise", "Tasse", "Pck.", "Dose",]
+    const div = document.createElement("div");
+    div.setAttribute("class", "create-ingredients-container");
+
+    const unitSelect = document.createElement("select");
+    unitSelect.setAttribute("name", "create-unit");
+    unitSelect.setAttribute("class", "create-unit");
+    units.forEach(unit => {
+        const option = document.createElement("option");
+        option.setAttribute("value", unit);
+        option.textContent = unit;
+        unitSelect.appendChild(option);
+    })
+
+    const ingredientInput = document.createElement("input");
+    ingredientInput.setAttribute("type", "text");
+    ingredientInput.setAttribute("name", "create-ingredient");
+    ingredientInput.setAttribute("placeholder", "Zutat");
+    ingredientInput.setAttribute("class", "create-ingredient");
+
+    const amountInput = document.createElement("input");
+    amountInput.setAttribute("type", "number");
+    amountInput.setAttribute("name", "create-amount");
+    amountInput.setAttribute("placeholder", "Menge");
+
+    div.appendChild(ingredientInput);
+    div.appendChild(amountInput);
+    div.appendChild(unitSelect);
+    return div;
+}
+addIngredientButton.addEventListener("click", (event) => {
+    const ingredientsContainer = document.getElementById("ingredientsContainer");
+    ingredientsContainer.appendChild(createIngredientInput());
+})
 
 
 form.addEventListener("submit", (event) => {
@@ -10,29 +48,39 @@ form.addEventListener("submit", (event) => {
 
     // getting the recipes out of the local storage
     const recipeList = getRecipeListFromLocalStorage();
-
+    const units = recipeList.map(recipe => recipe.extendedIngredients.map(ingredient => ingredient.unit))
+    console.log(units)
     // getting the values from the form
-    const ingredients = document.getElementById("create-ingredients");
+
+    const ingredientsContainer = document.getElementsByClassName("create-ingredients-container");
+    const extendedIngredients = Array.from(ingredientsContainer).map(element => {
+        return {name: element.children[0].value, amount: element.children[1].value, unit: element.children[2].value}
+    });
     const title = document.getElementById("title");
     const instructions = document.getElementById("instructions");
-    const dietType = document.getElementById("create-diet-type");
-    const mealType = document.getElementById(`create-meal-type`);
+    const dietTypeElements = document.querySelectorAll(`input[name="create-diet-type"]:checked`)
+    const dietTypes = Array.from(dietTypeElements).map(element => element.value);
+    const mealTypeElements = document.querySelectorAll(`input[name="create-meal-type"]:checked`)
+    const mealTypes = Array.from(mealTypeElements).map(element => element.value);
     const cookingTime = document.getElementById("cooking-time");
-    console.log(ingredients.value, title.value,  instructions.value, dietType.value, mealType.value)
-
+    const summary = document.getElementById("summary");
+    const diets = recipeList.map(recipe => recipe.diets)
+    const types = recipeList.map(recipe => recipe.dishTypes)
     // creating the recipe object
     const recipe = {
         "title": title.value,
-        "analyzedInstructions": instructions.value,
-        "diets": dietType.value,
-        "mealType": mealType.value,
-        "extendedIngredients": ingredients.value.split(","),
-        "cookingMinutes": cookingTime.value
+        "summary": summary.value,
+        "instructions": instructions.value,
+        "diets": dietTypes,
+        "dishTypes": mealTypes,
+        "extendedIngredients": extendedIngredients,
+        "readyInMinutes": cookingTime.value
     }
 
+    console.log(recipe)
 
     // pushing the recipe into the recipeList and storing it in the local storage
-    recipeList.push(recipe);
-    storeRecipeListInLocalStorage(recipeList);
+    // recipeList.push(recipe);
+    // storeRecipeListInLocalStorage(recipeList);
 
 });
